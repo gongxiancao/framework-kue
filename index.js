aeaf091c24e1b4e1693389aa4d65c4b95924f104
GongXian Cao <gongxiancao@hotmail.com>
2016-04-20 17:30:06 +0800

    use promise

--- index.js
+++ index.js
@@ -17,102 +17,105 @@ module.exports.connections = {
 **/
 
 'use strict';
-
+var Promise = require('bluebird');
 var kue = require('kue');
-var kueConfig = framework.config.kue;
-if(!kueConfig) {
-  throw new Error('require kue config');
-}
 
-var connectionName = kueConfig.connectionName;
-if(!connectionName) {
-  throw new Error('require kue connection name');
-}
-
-var connectionConfig = framework.config.connections[connectionName] || {};
-var queue = kue.createQueue({
-  prefix: kueConfig.prefix,
-  redis: {
-    port: connectionConfig.port || 6379,
-    host: connectionConfig.host || '127.0.0.1',
-    auth: connectionConfig.password || ''
+function lift (done) {
+  var kueConfig = framework.config.kue;
+  if(!kueConfig) {
+    throw new Error('require kue config');
   }
-});
 
-var frameworkKue = {
-  queue: queue,
-  //create a new job and insert it into kue
-  createJob: function (name, data, options, done) {
-    options = options || {};
-    var job = queue.create(name, data);
+  var connectionName = kueConfig.connection;
+  if(!connectionName) {
+    throw new Error('require kue connection name');
+  }
 
-    if(options.delay) {
-      job.delay(options.delay);
+  var connectionConfig = framework.config.connections[connectionName] || {};
+  var queue = kue.createQueue({
+    prefix: kueConfig.prefix,
+    redis: {
+      port: connectionConfig.port || 6379,
+      host: connectionConfig.host || '127.0.0.1',
+      auth: connectionConfig.password || ''
     }
-    if(options.priority) {
-      job.priority(options.priority);
-    }
-    if(options.attempts) {
-      job.attempts(options.attempts);
-    }
-    if(options.backoff) {
-      job.backoff(options.backoff);
-    }
-    options.ttl = options.ttl || 1000 * 60 * 60 * 24;
-    if(options.ttl) {
-      job.ttl(options.ttl);
-    }
+  });
 
-    job.save(function (err) {
-      if(done) {
-        done(err, job);
+  framework.kue = {
+    queue: queue,
+    //create a new job and insert it into kue
+    createJob: function (name, data, options, done) {
+      options = options || {};
+      var job = queue.create(name, data);
+
+      if(options.delay) {
+        job.delay(options.delay);
       }
-    });
-    return job;
-  },
-  queryJobs: function (name, state, dataFilter, limit, done) {
-    kue.Job.rangeByType(name, state, 0, limit, 'asc', function (err, jobs) {
-      if(err) {
-        return done(err);
+      if(options.priority) {
+        job.priority(options.priority);
       }
-      done(null, _.filter(jobs, function (job) {
-        if(dataFilter) {
-          return _.isEqual(_.pick(job.data, Object.keys(dataFilter)), dataFilter);
+      if(options.attempts) {
+        job.attempts(options.attempts);
+      }
+      if(options.backoff) {
+        job.backoff(options.backoff);
+      }
+      options.ttl = options.ttl || 1000 * 60 * 60 * 24;
+      if(options.ttl) {
+        job.ttl(options.ttl);
+      }
+
+      job.save(function (err) {
+        console.log('xxxxyyy', job);
+        if(done) {
+          done(err, job);
         }
-        return true;
-      }));
-    });
-  },
-  getJob: function (id, done) {
-    kue.Job.get(id, done);
-  },
-  processJob: function (name, options, callback) {
-    if(options instanceof Function) {
-      callback = options;
-      options = {};
-    }
-    options = options || {};
-
-    var concurrency = options.concurrency || 1;
-
-    queue.process(name, concurrency, function (job, done) {
-      callback(job, done);
-    });
-  },
-
-  clear: function (done) {
-    kue.Job.rangeByState('complete', 0, 10000, 'asc', function (err, jobs) {
-      if(err) {
-        return done(err);
+      });
+      return job;
+    },
+    queryJobs: function (name, state, dataFilter, limit, done) {
+      kue.Job.rangeByType(name, state, 0, limit, 'asc', function (err, jobs) {
+        if(err) {
+          return done(err);
+        }
+        done(null, _.filter(jobs, function (job) {
+          if(dataFilter) {
+            return _.isEqual(_.pick(job.data, Object.keys(dataFilter)), dataFilter);
+          }
+          return true;
+        }));
+      });
+    },
+    getJob: function (id, done) {
+      kue.Job.get(id, done);
+    },
+    processJob: function (name, options, callback) {
+      if(options instanceof Function) {
+        callback = options;
+        options = {};
       }
-      async.each(jobs, function (job, done) {
-        job.remove(done);
-      }, done);
-    });
-  }
-};
+      options = options || {};
 
-module.exports = function (done) {
-  framework.kue = frameworkKue;
+      var concurrency = options.concurrency || 1;
+
+      queue.process(name, concurrency, function (job, done) {
+        callback(job, done);
+      });
+    },
+
+    clear: function (done) {
+      kue.Job.rangeByState('complete', 0, 10000, 'asc', function (err, jobs) {
+        if(err) {
+          return done(err);
+        }
+        async.each(jobs, function (job, done) {
+          job.remove(done);
+        }, done);
+      });
+    }
+  };
+
   done();
 };
+
+module.exports = Promise.promisify(lift);
